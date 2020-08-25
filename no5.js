e30f7e5a8a58cc26772bc908c9d9a4a7bd090bc3
uhei <50701988+kyohayata@users.noreply.github.com>
2020-08-25 18:56:07 +0900

    Delete no5.js

--- no5.js
+++
@@ -1,8 +0,0 @@
-//.bottomのhover
-$(function(){
-  $('.middle li>img').hover(function(){
-    $('.middle li>.popular-text').css('opacity','0.5').css('transition','0.5s');
-  },function(){
-    $('.middle li>.popular-text').css('opacity','1').css('transition','0.5s');
-  });
-});
